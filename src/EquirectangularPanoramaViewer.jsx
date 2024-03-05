d5bf957d145fbfa1bb39b93385ac842b963792b7
rajkumar82 <rajkumar.r82@gmail.com>
2024-03-05 19:32:04 +0530

    implemented blurring

--- src/EquirectangularPanoramaViewer.jsx
+++ src/EquirectangularPanoramaViewer.jsx
@@ -1,35 +1,74 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
-import { useFrame, useLoader } from '@react-three/fiber'
+import { useLoader, useFrame } from '@react-three/fiber'
+import { useRef, useState } from 'react'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
-
 import * as THREE from 'three'
 import './EquirectangularPanoramaViewer.css'
-import { useState } from 'react'
+
+import { OrbitControls } from '@react-three/drei'
+
 
 export default function EquirectangularPanoramaViewer({ fileName, currentState }) {
 
-    const [meshes, setMeshes] = useState([{
-        position: [0, 0, 0]
-    }]);
+
+    useFrame((state, delta) => {
+        //console.log(delta)
+        delta;
+
+    })
+
+    const meshRef = useRef();
+
+
+    const [meshes, setMeshes] = useState([
+
+    ]);
 
     const colorMap = useLoader(TextureLoader, fileName)
     colorMap.colorSpace = 'srgb'
 
-    useFrame((state, delta) => {
-        delta;
-    })
+    //const [isDragging, setIsDragging] = useState(false);
+    //const [previousMousePosition, setPreviousMousePosition] = useState([0, 0]);
+
+    // const handleMouseDown = (event) => {
+    //     setIsDragging(true);
+    //     setPreviousMousePosition([event.clientX, event.clientY]);
+    // };
+
+    // const handleMouseUp = () => {
+    //     setIsDragging(false);
+    // };
+
+    // const handleMouseMove = (event) => {
 
 
+
+    //     if (isDragging) {
+    //         const deltaMousePosition = [
+    //             event.clientX - previousMousePosition[0],
+    //             event.clientY - previousMousePosition[1]
+    //         ];
+
+    //         meshRef.current.rotation.y += deltaMousePosition[0] * 0.01;
+    //         meshRef.current.rotation.x += deltaMousePosition[1] * 0.01;
+
+    //         setPreviousMousePosition([event.clientX, event.clientY]);
+    //     }
+    // };
 
     const clickEventHandler = (event) => {
 
-        if (currentState === 'blur') {
+
+        console.log(meshes.length);
+
+        if (currentState === 'blur' && event.ctrlKey) {
             //Add a blurring mesh        
-            //console.log(event.point);
+            console.log(event.point);
             const newMeshes = [...meshes];
             newMeshes.push({ position: event.point });
-            setMeshes(newMeshes)
+            setMeshes(newMeshes);
+            return;
         }
 
     }
@@ -43,21 +82,40 @@ export default function EquirectangularPanoramaViewer({ fileName, currentState }
     return (
         <>
 
+            <OrbitControls 
+            minDistance={0} // Minimum distance
+            maxDistance={80} // Maximum distance
+            />
 
-            <mesh onClick={clickEventHandler}>
-                <sphereGeometry args={[800, 100, 100]} />
-                <meshBasicMaterial map={colorMap} side={THREE.BackSide} />
-            </mesh>
+            <group
 
-            {meshes.map((mesh, index) => (
-                <mesh key={index} position={mesh.position}>
-                    <sphereGeometry args={[120, 16, 16]} />
-                    <meshPhysicalMaterial
-                        transmission={1}
-                        roughness={0.4}
-                    />
+                ref={meshRef}
+
+                // onPointerDown={handleMouseDown}
+                // onPointerUp={handleMouseUp}
+                // onPointerMove={handleMouseMove}
+                onClick={clickEventHandler}
+                onDoubleClick={doubleClickEventHandler}
+
+
+            >
+
+                <mesh>
+                    <sphereGeometry args={[120, 128, 128]} />
+                    <meshBasicMaterial map={colorMap} side={THREE.BackSide} />
                 </mesh>
-            ))}
+
+                {meshes.map((mesh, index) => (
+                    <mesh key={index} position={mesh.position}>
+                        <sphereGeometry args={[10, 16, 16]} />
+                        <meshPhysicalMaterial
+                            transmission={1}
+                            roughness={0.4}
+                        />
+                    </mesh>
+                ))}
+
+            </group>
         </>
     )
 }
